1712382eebe4088605de4a29f28bb409d4c3bda1
Max Ogden <max@maxogden.com>
2011-05-14 18:12:56 -0700

    adding search box

--- app.js
+++ app.js
@@ -46,10 +46,40 @@ ddoc.spatial = {
     if(doc.name && doc.geometry) {        
       emit(doc.geometry, doc.name);
     }
+  },
+  full: function(doc){
+  	if(doc.geometry){
+  		emit(doc.geometry, doc);
+  	}
   }
 }
 
 ddoc.lists = {
+  
+  geojson: function(head, req) {
+      var row, out, sep = '\n';
+
+      // Send the same Content-Type as CouchDB would
+      if (req.headers.Accept.indexOf('application/json')!=-1)
+        start({"headers":{"Content-Type" : "application/json"}});
+      else
+        start({"headers":{"Content-Type" : "text/plain"}});
+
+      if ('callback' in req.query) send(req.query['callback'] + "(");
+
+      send('{"type": "FeatureCollection", "features":[');
+      while (row = getRow()) {
+          out = '{"type": "Feature", "geometry": ' + JSON.stringify(row.value.geometry);
+          delete row.value.geometry;
+          out += ', "properties": ' + JSON.stringify(row.value) + '}';
+
+          send(sep + out);
+          sep = ',\n';
+      }
+      send("]}");
+      if ('callback' in req.query) send(")");
+
+  },
 
   search : function(head, req) {
     var row;
@@ -83,7 +113,7 @@ ddoc.lists = {
       if(terms.some(function(term) {
         return (query.indexOf(term) != -1);
       })) {
-        send(prefix + '"' + row.id + '"');
+        send(prefix + JSON.stringify(row.value));
         if(!prefix) prefix = ',\n';
       }
     }

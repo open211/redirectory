489b3e9e9bb11a5f73d253dc543710c850d80296
Brian T. Rice <briantrice@gmail.com>
2011-06-05 11:07:28 -0700

    Tropo script updates to parse out cities in addition to ZIP codes. Search results disabled for the moment.

--- tropo/search.rb
+++ tropo/search.rb
@@ -2,49 +2,62 @@ require 'rubygems'
 require 'json'
 require 'net/http'
 require 'uri'
+require 'open-uri'
+require 'set'
 
-outbound = !!$message
-
-api_host = "rectangl.es"
-api_uri = "http://#{api_host}/api"
+api_host = "smalldata.org:9200"
+api_uri = "http://#{api_host}/social_services/social_services/_search"
 
 result_limit = 5
 
-unless outbound
+def newline_separated_file_to_set name
+  things = Set.new
+  open name do |f|
+    f.lines.each do |thing|
+      things.add thing.strip.downcase
+    end
+  end
+  things
+end
+
+def parse_query words
+  unimportant_words = ['and', 'or']
+  zipcodes, keywords = words.partition {|item| item =~ /^[0-9]{5}$/}
+  cities = newline_separated_file_to_set 'http://hosting.tropo.com/71814/www/city_names.txt'
+  cities_for_query, keywords = keywords.partition {|item| cities.include? item.strip.downcase}
+  keywords.reject! {|item| unimportant_words.include?(item)}
+  return zipcodes, cities_for_query, keywords
+end
+
+unless $message
   if $currentCall.channel == "TEXT"
     input = $currentCall.initialText
     network = $currentCall.network
-    zipcode = ''
-    bbox = ''
-    words = input.split(' ')
-    unimportant_words = ['and', 'or']
-    words.each do |item|
-      if item =~ /^[0-9]{5}$/
-        zipcode = item
-      end
+    words = input.split
+    zipcodes, cities_for_query, keywords = parse_query words
+    while zipcodes.empty? and cities_for_query.empty? and keywords.empty?
+      message = "We expect input to include a ZIP code or city name (city names with spaces are unsupported currently, sorry.), along with a list of keywords to search for.  Please try again."
+      words = ask(message).split
+      zipcodes, cities_for_query, keywords = parse_query words
     end
-    keywords = words.reject do |item|
-      unimportant_words.include?(item) || item == zipcode
-    end
-    unless network == 'SMS'
-      say "Welcome to the Redirectory! Finding " + keywords.join(", ") + " in ZIP: #{zipcode}"
-    end
-    #extra_params = {
-    #  :message => $currentCall.initialText,
-    #  :incoming_number => $currentCall.calledID,
-    #  :origin_number => $currentCall.callerID
-    #}
-    uri = URI.parse(api_uri + "/zip?key=" + '%22' + zipcode + '%22')
-    unless zipcode.empty?
-      zip_response = Net::HTTP.get_response(uri)
-      results = JSON.parse(zip_response.body)
-      bbox = results["rows"][0]["value"]["bbox"]
-    end
-    query = keywords.join(',')
-    uri = URI.parse("#{api_uri}/search?bbox=#{bbox}&query=#{query}")
-    search_response = Net::HTTP.get_response(uri)
-    results = JSON.parse(search_response.body)
-    say results["rows"].first(result_limit).join("\n")
+    location_feedback = (zipcodes + cities_for_query).join(', ')
+    say "Searching for #{keywords.join(', ')} in #{location_feedback}"
+    # #extra_params = {
+    # #  :message => $currentCall.initialText,
+    # #  :incoming_number => $currentCall.calledID,
+    # #  :origin_number => $currentCall.callerID
+    # #}
+    # uri = URI.parse(api_uri + "/zip?key=" + '%22' + zipcode + '%22')
+    # unless zipcode.empty?
+    #   zip_response = Net::HTTP.get_response(uri)
+    #   results = JSON.parse(zip_response.body)
+    #   bbox = results["rows"][0]["value"]["bbox"]
+    # end
+    # query = keywords.join(',')
+    # uri = URI.parse("#{api_uri}/search?bbox=#{bbox}&query=#{query}")
+    # search_response = Net::HTTP.get_response(uri)
+    # results = JSON.parse(search_response.body)
+    # say results["rows"].first(result_limit).join("\n")
   elsif $currentCall.channel == "VOICE"
     callerID = $currentCall.callerID
     record "Welcome to the Redirectory! What problems need solving?", {
